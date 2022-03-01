1ccbe3f7c54b264edec624d5e5d06d9f2c86f68f
ALEXIE NARCISO <NA.LEXIE.AN@gmail.com>
2022-03-01 10:12:37 -0800

    update repo for part 1,2,3

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyBhtu5iNecVdZXsHJF0Nt6nMEFCrgCbud4",
+    authDomain: "fir-comp1800-e53f4.firebaseapp.com",
+    projectId: "fir-comp1800-e53f4",
+    storageBucket: "fir-comp1800-e53f4.appspot.com",
+    messagingSenderId: "1040825997913",
+    appId: "1:1040825997913:web:2dee5231c100571b48ef48",
+    measurementId: "G-V43XD6F0QG"
 };
 
 //--------------------------------------------
